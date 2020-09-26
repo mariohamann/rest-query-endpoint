64a8951fc66ff07001806997da306cc0a58d7313
Mario <mariovhamann@gmail.com>
2020-09-26 17:12:36 +0200

    (init) create endpoint

--- rest-query-endpoint.php
+++ rest-query-endpoint.php
@@ -17,3 +17,56 @@ namespace MarioHamann\RestQueryEndpoint;
 if ( ! defined( 'ABSPATH' ) ) exit;
 /* Nach dieser Zeile den Code einfügen*/
 
+QueryEndpoint::get_instance();
+
+class QueryEndpoint {
+	public static function get_instance() {
+	static $instance = null;
+		if (is_null($instance)) {
+			$instance = new self();
+		}
+		return $instance;
+	}
+
+	private function __construct() {
+		add_action( 'rest_api_init', array($this, 'registerQueryRoute'));
+	}
+
+	public function registerQueryRoute() {
+
+		// Register endpoint
+		register_rest_route('wp/v2', '/query',
+			array(
+				'methods' => 'GET',
+				'callback' => function ($data) {
+					// Get JSON
+					$json = $data->get_param( 'json' ) ?? '';
+					$params = json_decode($json);
+
+					// Check if JSON is valid JSON
+					if (!(json_last_error() === JSON_ERROR_NONE)) {
+						return  __('ERROR: Malformed JSON.', 'wp');
+					}
+
+					// Set defaults
+					$query = $params->query ?? 'WP_Query';
+					$args = $params->args ?? array("post_type" => "post");
+					
+					// Create the Query
+					if($query === 'WP_Query'){
+						$query = new \WP_Query( $args );
+						$objects = $query->posts;
+					}
+					elseif($query === 'WP_Term_Query'){
+						$query = new \WP_Term_Query( $args );
+						$objects = $query->terms;
+					}
+					else{
+						return __('ERROR: Unknown Query.', 'wp');
+					}
+					return $objects;
+				}
+			)
+		);
+	}
+}
