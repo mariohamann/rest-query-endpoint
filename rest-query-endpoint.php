03a88111d08de8121b0cf1ef51b80c5f616c1937
Mario <mariovhamann@gmail.com>
2020-09-26 15:15:14 +0200

    first commit

---
+++ rest-query-endpoint.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: Rest Query Endpoint (Experiment)
+ * Plugin URI: https://github.com/mariohamann/rest-query-endpoint
+ * Version: 1.0.0
+ * Description: Experiment to create an Query-endpoint for Gutenberg
+ * License: GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Author: mariohamann
+ * Author URI: https://github.com/mariohamann
+ */
+
+namespace MarioHamann\RestQueryEndpoint;
+
+/* Verbiete den direkten Zugriff auf die Plugin-Datei */
+if ( ! defined( 'ABSPATH' ) ) exit;
+/* Nach dieser Zeile den Code einfügen*/
+
